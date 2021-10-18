858be4ed96dffc7e3f76091344b1028cc9853408
sungkihwan <singahu@naver.com>
2021-10-19 00:41:13 +0900

    quickSort

--- src/main/java/study/sorting/controller/SortingContorller.java
+++ src/main/java/study/sorting/controller/SortingContorller.java
@@ -1,13 +1,15 @@
 package study.sorting.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import study.sorting.service.SortingService;
 
 @RestController
 @RequestMapping
+@RequiredArgsConstructor
 public class SortingContorller {
+
+    private final SortingService sortingService;
 
     @GetMapping("/v1/sorting/{type}")
     public int[] sorting(@PathVariable("type") String type, @RequestParam(value="values") int[] values) {
@@ -16,71 +18,17 @@ public class SortingContorller {
 
         switch (type) {
             case "merge":
-                mergeSort(arr, 0, arr.length - 1);
+                sortingService.myMergeSort(arr);
+                break;
             case "quick":
+                sortingService.myQuickSort(arr);
+                break;
             case "heap" :
+                sortingService.myHeapSort(arr);
+                break;
         }
 
         return arr;
     }
 
-    private void merge(int[] arr, int left, int mid, int right){
-//        List<Integer> temp = new ArrayList<>();
-        int[] temp = new int[2000];
-
-        int i = left;
-        int j = mid + 1;
-        int k = 0;
-
-        while (i <= mid && j <= right) {
-            if (arr[i] <= arr[j]){
-                temp[k] = arr[i];
-                k++;
-                i++;
-            } else {
-                temp[k] = arr[j];
-                j++;
-                k++;
-            }
-        }
-
-        while (i <= mid) {
-            temp[k] = arr[i];
-            k++;
-            i++;
-        }
-
-        while (j <= right) {
-            temp[k] = arr[j];
-            k++;
-            j++;
-        }
-
-        k--;
-        while (k >= 0) {
-            arr[left + k] = temp[k];
-            k--;
-        }
-    }
-
-    private void mergeSort(int[] arr, int left, int right) {
-        if(left<right){
-            int mid = (left + right) / 2;
-
-            mergeSort(arr, left, mid);
-            mergeSort(arr, mid+1, right);
-
-            merge(arr, left, mid, right);
-        }else{
-            return;
-        }
-    }
-
-    private void quickSort(int[] arr) {
-        return;
-    }
-
-    private void heapSort(int[] arr) {
-        return;
-    }
 }
---
+++ src/main/java/study/sorting/service/SortingService.java
@@ -0,0 +1,114 @@
+package study.sorting.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Service
+public class SortingService {
+
+    public void myMergeSort(int[] arr) {
+        mergeSort(arr, 0, arr.length - 1);
+    }
+
+    // 분할
+    private void mergeSort(int[] arr, int left, int right) {
+        if(left<right){
+            int mid = (left + right) / 2;
+
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid+1, right);
+
+            merge(arr, left, mid, right);
+        }else{
+            return;
+        }
+    }
+
+    // 정복
+    private void merge(int[] arr, int left, int mid, int right){
+        List<Integer> temp = new ArrayList<>();
+
+        int i = left;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]){
+                temp.add(k,arr[i]);
+                k++;
+                i++;
+            } else {
+                temp.add(k,arr[j]);
+                j++;
+                k++;
+            }
+        }
+
+        while (i <= mid) {
+            temp.add(k,arr[i]);
+            k++;
+            i++;
+        }
+
+        while (j <= right) {
+            temp.add(k,arr[j]);
+            k++;
+            j++;
+        }
+
+        k--;
+        while (k >= 0) {
+            arr[left + k] = temp.get(k);
+            k--;
+        }
+    }
+
+    public void myQuickSort(int[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private void quickSort(int[] arr, int left, int right) {
+
+        int index;
+
+        if (left < right) {
+            index = partition(arr, left, right);
+            quickSort(arr, left, index-1);
+            quickSort(arr, index+1, right);
+        }
+    }
+
+    private int partition(int[] arr, int left, int right) {
+
+        int random = (int)(Math.random() * (right - left) + left);
+
+        int temp = arr[random];
+        arr[random] = arr[right];
+        arr[right] = temp;
+
+        int pivot = arr[right];
+        int index = left;
+
+        for (int i = left; i <right; i++) {
+            if (arr[i] <= pivot) {
+                temp = arr[index];
+                arr[index] = arr[i];
+                arr[i] = temp;
+                index++;
+            }
+        }
+
+        temp = arr[index];
+        arr[index] = arr[right];
+        arr[right] = temp;
+
+        return index;
+    }
+
+    public void myHeapSort(int[] arr) {
+    }
+
+}
